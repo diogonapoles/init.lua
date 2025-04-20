abc832bb1c9a68afca041312fd04f979dc7e3cc1
diogonapoles <napolesdr@gmail.com>
2025-04-20 16:53:16 +0100

    feat(telescope): general improvements
    - enabled fzf and ui-select

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -3,6 +3,7 @@ return {
 	tag = "0.1.8",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
 		"nvim-telescope/telescope-ui-select.nvim",
 		{
 			"nvim-telescope/telescope-fzf-native.nvim",
@@ -12,15 +13,22 @@ return {
 
 	config = function()
 		local set = vim.keymap.set
-		require("telescope").setup({
+
+		local builtin = require("telescope.builtin")
+		set("n", "<space>ff", builtin.find_files, { desc = "Find files on the current directory" })
+		set("n", "<space>fs", builtin.live_grep, { desc = "Find string" })
+
+		local telescope = require("telescope")
+		telescope.setup({
 			extensions = {
 				wrap_results = true,
 			},
+
+			find_files = {
+				hidden = true,
+			},
 		})
-
-		local builtin = require("telescope.builtin")
-
-		set("n", "<space>ff", builtin.find_files, { desc = "Find files on the current directory" })
-		set("n", "<space>fs", builtin.live_grep, { desc = "Find string" })
+		telescope.load_extension("fzf")
+		telescope.load_extension("ui-select")
 	end,
 }
