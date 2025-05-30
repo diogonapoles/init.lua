0ac76c4dbff9cd799654067801c754aa05ae5878
diogonapoles <napolesdr@gmail.com>
2025-05-30 15:43:23 +0100

    feat(options): enhanced options file

--- lua/config/core/options.lua
+++ lua/config/core/options.lua
@@ -6,26 +6,36 @@ g.mapleader = " "
 g.maplocalleader = "\\"
 
 -- clipboard
-opt.clipboard = "unnamedplus"
+opt.clipboard:append("unnamedplus")
 
 -- search
 opt.smartcase = true
+opt.incsearch = true
+opt.inccommand = "split"
 opt.ignorecase = true
 
 -- personal
 opt.title = true
 opt.number = true
 opt.relativenumber = true
-opt.inccommand = "split"
-opt.signcolumn = "yes"
 opt.swapfile = false
-opt.wrap = true
 opt.linebreak = true
 
+-- look
+opt.termguicolors = true
+opt.background = "dark"
+opt.scrolloff = 8
+opt.signcolumn = "yes"
+
 -- windows
-opt.splitbelow = true
 opt.splitright = true
+opt.splitbelow = true
 
 -- indentation
 opt.tabstop = indentation
+opt.softtabstop = indentation
 opt.shiftwidth = indentation
+opt.expandtab = true
+opt.autoindent = true
+opt.smartindent = true
+opt.wrap = false
