8a9da8010a5acd14a5b4cd1f8e3c594c9efabd8c
diogonapoles <napolesdr@gmail.com>
2025-07-20 12:46:03 +0100

    feat(colorscheme): disabled transparent background

--- lua/diogonapoles/plugins/colorscheme.lua
+++ lua/diogonapoles/plugins/colorscheme.lua
@@ -2,10 +2,10 @@ return {
   'sainnhe/gruvbox-material',
   priority = 1000,
   config = function()
-    vim.g.gruvbox_material_transparent_background = 1
-    vim.g.gruvbox_material_background = 'hard'
-    vim.g.gruvbox_material_foreground = 'mix'
-    vim.g.gruvbox_material_ui_contrast = 'hard'
+    vim.g.gruvbox_material_transparent_background = 0
+    vim.g.gruvbox_material_background = 'medium'
+    vim.g.gruvbox_material_foreground = 'medium'
+    vim.g.gruvbox_material_ui_contrast = 'medium'
 
     vim.cmd.colorscheme 'gruvbox-material'
   end,
