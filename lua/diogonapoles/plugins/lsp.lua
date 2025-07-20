9c6afb03feaa86ba54a19982c9b5e32d9831e702
diogonapoles <napolesdr@gmail.com>
2025-07-20 12:49:08 +0100

    feat(formatter): enabled prettier for ts files

--- lua/diogonapoles/plugins/lsp.lua
+++ lua/diogonapoles/plugins/lsp.lua
@@ -169,6 +169,7 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
